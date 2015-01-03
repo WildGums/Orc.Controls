8093c78774de7f7f43b135691e80342a00dffd98
Tanya Pristupova <zyrk@zyrk.net>
2015-01-03 11:28:44 +0100

    Adds SuggestionListService

---
+++ src/Orc.Controls/Services/Interfaces/ISuggestionListService.cs
@@ -0,0 +1,9 @@
+﻿namespace Orc.Controls.Services.Interfaces
+{
+    using System;
+
+    public interface ISuggestionListService
+    {
+        string[] GetSuggestionList(DateTime dateTime, DateTimePart editablePart);
+    }
+}
---
+++ src/Orc.Controls/Services/SuggestionListService.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SuggestionListService.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls.Services
+{
+    using System;
+    using Interfaces;
+
+    public class SuggestionListService : ISuggestionListService
+    {
+        #region ISuggestionListService Members
+        public string[] GetSuggestionList(DateTime dateTime, DateTimePart editablePart)
+        {
+            return new[] {"qq", "ee", "ff"};
+        }
+        #endregion
+    }
+}
