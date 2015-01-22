6a4bfd2e775ce92874c8684a8c6a794965745aa4
Tanya Pristupova <zyrk@zyrk.net>
2015-01-22 16:13:16 +0100

    Adds FilterBoxControl

---
+++ src/Orc.Controls/ViewModels/FilterBoxViewModel.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FilterBoxViewModel.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls.ViewModels
+{
+    using Catel.MVVM;
+
+    public class FilterBoxViewModel : ViewModelBase
+    {
+
+    }
+}
---
+++ src/Orc.Controls/Views/FilterBoxControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FilterBoxControl.xaml.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls
+{
+    using Catel.MVVM.Views;
+
+    /// <summary>
+    /// Interaction logic for FilterBox.xaml
+    /// </summary>
+    public partial class FilterBoxControl
+    {
+        #region Constructors
+        static FilterBoxControl()
+        {
+            typeof (FilterBoxControl).AutoDetectViewPropertiesToSubscribe();
+        }
+
+        public FilterBoxControl()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
