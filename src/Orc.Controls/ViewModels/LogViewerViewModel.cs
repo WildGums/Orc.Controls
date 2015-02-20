2b3908011603a47e09f027e80bb78ab08cb8ef03
Tanya Pristupova <zyrk@zyrk.net>
2015-02-20 11:22:49 +0100

    Adds  LogViewerControl

---
+++ src/Orc.Controls/ViewModels/LogViewerViewModel.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogViewerViewModel.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls.ViewModels
+{
+    using Catel.MVVM;
+
+    public class LogViewerViewModel : ViewModelBase
+    {
+    }
+}
---
+++ src/Orc.Controls/Views/LogViewerControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogViewerControl.xaml.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls
+{
+    /// <summary>
+    /// Interaction logic for LogViewerControl.xaml.
+    /// </summary>
+    public partial class LogViewerControl
+    {
+        #region Constructors
+        public LogViewerControl()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
