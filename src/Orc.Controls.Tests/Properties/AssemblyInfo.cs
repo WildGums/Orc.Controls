d67ce68a5f42f4cef13e128d6f520fda9c6a6023
Geert van Horrik <geert@catenalogic.com>
2015-02-23 14:40:38 +0100

    Small improvements

--- src/Orc.Controls.Tests/Properties/AssemblyInfo.cs
+++ src/Orc.Controls.Tests/Properties/AssemblyInfo.cs
@@ -1,36 +1,12 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Simulation Modelling Services">
+//   Copyright (c) 2008 - 2014 Simulation Modelling Services. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+
+using System.Reflection;
+
 [assembly: AssemblyTitle("Orc.Controls.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Orc.Controls.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("44eba4e0-9e09-4919-8c48-4e03199b846c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("Orc.Controls.Tests")]
--- src/Orc.Controls/Properties/AssemblyInfo.cs
+++ src/Orc.Controls/Properties/AssemblyInfo.cs
@@ -1,36 +1,22 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Simulation Modelling Services">
+//   Copyright (c) 2008 - 2014 Simulation Modelling Services. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+
+using System.Reflection;
+using System.Windows.Markup;
+
 [assembly: AssemblyTitle("Orc.Controls")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Orc.Controls")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Orc.Controls")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c536cb77-5e47-483d-a425-45f5f65ec158")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: XmlnsPrefix("http://www.wildgums.net.au/orc", "orc")]
+//[assembly: XmlnsDefinition("http://www.wildgums.net.au/orc", "Orc.Behaviors")]
+//[assembly: XmlnsDefinition("http://www.wildgums.net.au/orc", "Orc.Converters")]
+[assembly: XmlnsDefinition("http://www.wildgums.net.au/orc", "Orc.Controls")]
+//[assembly: XmlnsDefinition("http://www.wildgums.net.au/orc", "Orc.Fonts")]
+//[assembly: XmlnsDefinition("http://www.wildgums.net.au/orc", "Orc.Markup")]
+//[assembly: XmlnsDefinition("http://www.wildgums.net.au/orc", "Orc.Views")]
+//[assembly: XmlnsDefinition("http://www.wildgums.net.au/orc", "Orc.Windows")]
