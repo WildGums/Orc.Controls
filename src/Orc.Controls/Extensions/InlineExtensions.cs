774404f656bd9dc72a0ab6c76437784bc0650833
Maxim Khomutov <mkhomutov@gmail.com>
2015-03-13 21:25:41 +0300

    arguments null checking in extensions

--- src/Orc.Controls/Extensions/InlineExtensions.cs
+++ src/Orc.Controls/Extensions/InlineExtensions.cs
@@ -9,17 +9,23 @@ namespace Orc.Controls
 {
     using System.Collections.Generic;
     using System.Windows.Documents;
+    using Catel;
 
     public static class InlineExtensions
     {
         #region Methods
         public static Bold Bold(this Inline inline)
         {
+            Argument.IsNotNull(() => inline);
+
             return new Bold(inline);
         }
 
         public static Inline Append(this Inline inline, Inline inlineToAdd)
         {
+            Argument.IsNotNull(() => inline);
+            Argument.IsNotNull(() => inlineToAdd);
+
             var span = new Span(inline);
 
             span.Inlines.Add(inlineToAdd);
@@ -29,6 +35,9 @@ namespace Orc.Controls
 
         public static Inline AppendRange(this Inline inline, IEnumerable<Inline> inlines)
         {
+            Argument.IsNotNull(() => inline);
+            Argument.IsNotNull(() => inlines);
+
             var span = new Span(inline);
 
             span.Inlines.AddRange(inlines);
