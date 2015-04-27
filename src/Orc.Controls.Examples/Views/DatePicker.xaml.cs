bb0800ae5193f86570b2bd8664d02945af396106
Tanya Pristupova <zyrk@zyrk.net>
2015-04-27 17:54:37 +0200

    ORCOMP-111  Adds DatePicker control to example window

---
+++ src/Orc.Controls.Examples/Views/DatePicker.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Orc.Controls.Examples.Views
+{
+    using Catel.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for DateTimePicker.xaml.
+    /// </summary>
+    public partial class DatePicker : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DatePicker"/> class.
+        /// </summary>
+        public DatePicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
