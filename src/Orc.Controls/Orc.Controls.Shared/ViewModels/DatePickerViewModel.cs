922bff60cd4f18d44bf883ae41bcd6242ebf534a
Tanya Pristupova <zyrk@zyrk.net>
2015-04-27 17:52:16 +0200

    ORCOMP-111 Implements DatePicker control

---
+++ src/Orc.Controls/Orc.Controls.Shared/ViewModels/DatePickerViewModel.cs
@@ -0,0 +1,143 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DateTimePickerViewModel.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls
+{
+    using System;
+    using System.Windows.Media;
+    using Catel.MVVM;
+    using Extensions;
+
+    public class DatePickerViewModel : ViewModelBase
+    {
+        #region Fields
+        private Brush _accentColorBrushProperty;
+        private bool _showOptionsButton;
+        private Brush _highlightColorBrush;
+        private DateTime _value;
+        #endregion
+
+        #region Properties
+        public bool ShowOptionsButton
+        {
+            get { return _showOptionsButton; }
+            set
+            {
+                if (_showOptionsButton == value)
+                {
+                    return;
+                }
+
+                _showOptionsButton = value;
+
+                RaisePropertyChanged("ShowOptionsButton");
+            }
+        }
+
+        public Brush AccentColorBrush
+        {
+            get { return _accentColorBrushProperty; }
+            set
+            {
+                if (_accentColorBrushProperty == value)
+                {
+                    return;
+                }
+
+                _accentColorBrushProperty = value;
+                var accentColor = ((SolidColorBrush)AccentColorBrush).Color;
+                accentColor.CreateAccentColorResourceDictionary();
+                RaisePropertyChanged("AccentColorBrush");
+            }
+        }
+
+        public Brush HighlightColorBrush
+        {
+            get { return _highlightColorBrush; }
+            set
+            {
+                if (_highlightColorBrush == value)
+                {
+                    return;
+                }
+
+                _highlightColorBrush = value;
+
+                RaisePropertyChanged("HighlightColorBrush");
+            }
+        }
+
+        public DateTime Value
+        {
+            get { return _value; }
+            set
+            {
+                if (_value == value)
+                {
+                    return;
+                }
+
+                _value = value;
+
+                // Raise all property changed
+                RaisePropertyChanged(string.Empty);
+
+                // Required for mappings
+                RaisePropertyChanged("Value");
+            }
+        }
+
+        public int Year
+        {
+            get { return _value.Year; }
+            set
+            {
+                if (_value.Year == value)
+                {
+                    return;
+                }
+
+                Value = new DateTime(value, Month, Day);
+            }
+        }
+
+        public int Month
+        {
+            get { return _value.Month; }
+            set
+            {
+                if (_value.Month == value)
+                {
+                    return;
+                }
+                var daysInMonth = DateTime.DaysInMonth(Year, value);
+                if (Day <= daysInMonth)
+                {
+                    Value = new DateTime(Year, value, Day);
+                    return;
+                }
+                Day = daysInMonth;
+                Value = new DateTime(Year, value, daysInMonth);
+            }
+        }
+
+        public int Day
+        {
+            get { return _value.Day; }
+            set
+            {
+                if (_value.Day == value)
+                {
+                    return;
+                }
+
+                Value = new DateTime(Year, Month, value);
+            }
+        }
+        #endregion
+    }
+}
---
+++ src/Orc.Controls/Orc.Controls.Shared/Views/DatePickerControl.xaml.cs
@@ -0,0 +1,206 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DatePickerControl.xaml.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using Catel.MVVM.Views;
+
+    /// <summary>
+    /// Interaction logic for TimeSpanControl.xaml
+    /// </summary>
+    public partial class DatePickerControl
+    {
+        #region Fields
+        private readonly List<NumericTextBox> _numericTextBoxes;
+        private DateTimePart _activeDateTimePart;
+        #endregion
+
+        #region Constructors
+        static DatePickerControl()
+        {
+            typeof (DatePickerControl).AutoDetectViewPropertiesToSubscribe();
+        }
+
+        public DatePickerControl()
+        {
+            InitializeComponent();
+
+            _numericTextBoxes = new List<NumericTextBox>()
+            {
+                NumericTBDay,
+                NumericTBMonth,
+                NumericTBYear,
+            };
+
+            NumericTBDay.RightBoundReached += NumericTextBoxOnRightBoundReached;
+            NumericTBMonth.RightBoundReached += NumericTextBoxOnRightBoundReached;
+
+            NumericTBYear.LeftBoundReached += NumericTextBoxOnLeftBoundReached;
+            NumericTBMonth.LeftBoundReached += NumericTextBoxOnLeftBoundReached;
+        }
+        #endregion
+
+        #region Properties
+        [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewWins)]
+        public DateTime Value
+        {
+            get { return (DateTime) GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(DateTime), typeof(DatePickerControl),
+            new FrameworkPropertyMetadata(DateTime.Today, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewWins)]
+        public bool ShowOptionsButton
+        {
+            get { return (bool)GetValue(ShowOptionsButtonProperty); }
+            set { SetValue(ShowOptionsButtonProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowOptionsButtonProperty = DependencyProperty.Register("ShowOptionsButton", typeof(bool), 
+            typeof(DatePickerControl), new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewWins)]
+        public Brush AccentColorBrush
+        {
+            get { return (Brush)GetValue(AccentColorBrushProperty); }
+            set { SetValue(AccentColorBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty AccentColorBrushProperty = DependencyProperty.Register("AccentColorBrush", typeof(Brush), 
+            typeof(DatePickerControl), new FrameworkPropertyMetadata(Brushes.LightGray, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        #endregion
+
+        #region Methods
+        private void NumericTextBoxOnLeftBoundReached(object sender, EventArgs e)
+        {
+            var currentTextBoxIndex = _numericTextBoxes.IndexOf(sender as NumericTextBox);
+            var prevTextBox = _numericTextBoxes[currentTextBoxIndex - 1];
+
+            prevTextBox.CaretIndex = prevTextBox.Text.Length;
+            prevTextBox.Focus();
+        }
+
+        private void NumericTextBoxOnRightBoundReached(object sender, EventArgs eventArgs)
+        {
+            var currentTextBoxIndex = _numericTextBoxes.IndexOf(sender as NumericTextBox);
+            var nextTextBox = _numericTextBoxes[currentTextBoxIndex + 1];
+
+            nextTextBox.CaretIndex = 0;
+            nextTextBox.Focus();
+        }
+
+        private void ToggleButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            _activeDateTimePart = (DateTimePart) ((ToggleButton) sender).Tag;
+
+            var activeNumericTextBox = (NumericTextBox)FindName(_activeDateTimePart.GetDateTimePartName());
+            var activeToggleButton = (ToggleButton)FindName(_activeDateTimePart.GetDateTimePartToggleButtonName());
+
+            var dateTimePartHelper = new DateTimePartHelper(Value, _activeDateTimePart, activeNumericTextBox, activeToggleButton);
+            dateTimePartHelper.CreatePopup();
+        }
+        #endregion
+
+        private void NumericTBMonth_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var daysInMonth = DateTime.DaysInMonth(Value.Year, Value.Month);
+            NumericTBDay.MaxValue = daysInMonth;
+        }
+
+        private Calendar CreateCalendarPopupSource()
+        {
+            var calendar = new Calendar()
+            {
+                Margin = new Thickness(0, -3, 0, -3), 
+                DisplayDate = Value, 
+                SelectedDate = Value
+            };
+            
+            calendar.PreviewKeyDown += CalendarOnPreviewKeyDown;
+            calendar.SelectedDatesChanged += CalendarOnSelectedDatesChanged;
+
+            return calendar;
+        }
+
+        private void CalendarOnSelectedDatesChanged(object sender, SelectionChangedEventArgs selectionChangedEventArgs)
+        {
+            var calendar = (((Calendar)sender));
+            if (calendar.SelectedDate.HasValue)
+            {
+                UpdateDate(calendar.SelectedDate.Value);
+            }
+            ((Popup)calendar.Parent).IsOpen = false;
+            
+        }
+
+        private void CalendarOnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            var calendar = ((Calendar)sender);
+            if (e.Key == Key.Escape)
+            {
+                ((Popup)calendar.Parent).IsOpen = false;
+                NumericTBDay.Focus();
+                e.Handled = true;
+            }
+            if (e.Key == Key.Enter)
+            {
+                if (calendar.SelectedDate.HasValue)
+                {
+                    UpdateDate(calendar.SelectedDate.Value);
+                }
+                ((Popup)calendar.Parent).IsOpen = false;
+
+                e.Handled = true;
+            }
+        }
+
+        private void UpdateDate(DateTime date)
+        {
+            Value = new DateTime(date.Year, date.Month, date.Day);
+        }
+
+        private Popup CreateCalendarPopup()
+        {
+            var popup = new Popup
+            {
+                PlacementTarget = MainGrid,
+                Placement = PlacementMode.Bottom,
+                VerticalOffset = -4,
+                IsOpen = true,
+                StaysOpen = false,
+            };
+            return popup;
+        }
+
+        private void TodayButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DatePickerIcon.IsChecked = false;
+            UpdateDate(DateTime.Today.Date);
+        }
+
+
+        private void SelectDateButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DatePickerIcon.IsChecked = false;
+
+            var calendarPopup = CreateCalendarPopup();
+            var calendarPopupSource = CreateCalendarPopupSource();
+            calendarPopup.Child = calendarPopupSource;
+
+            calendarPopupSource.Focus();
+        }
+    }
+}
