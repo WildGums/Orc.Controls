661d87617b23d55569dc41497e0c0171210c81a4
Tanya Pristupova <zyrk@zyrk.net>
2015-05-04 12:29:23 +0200

    ORCOMP-112 Initial commit

---
+++ src/Orc.Controls/Orc.Controls.Shared/Views/LogUserControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogUserControl.xaml.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls
+{
+    using Catel.MVVM.Views;
+
+    public partial class LogUserControl
+    {
+        static LogUserControl()
+        {
+            typeof (LogUserControl).AutoDetectViewPropertiesToSubscribe();
+        }
+
+        public LogUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
