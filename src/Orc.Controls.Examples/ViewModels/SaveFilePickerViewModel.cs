b8c0680b68c6e833d893c18bb938f42a62a5187e
Tanya Pristupova <zyrk@zyrk.net>
2015-05-26 12:23:39 +0100

    ORCOMP-135 Adds SaveFilePickerView. Updates MainWindowView in example project

---
+++ src/Orc.Controls.Examples/ViewModels/SaveFilePickerViewModel.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SaveFilePickerViewModel.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls.Examples.ViewModels
+{
+    using Catel.MVVM;
+
+    public class SaveFilePickerViewModel : ViewModelBase
+    {
+        public SaveFilePickerViewModel()
+        {
+        }
+    }
+}
---
+++ src/Orc.Controls.Examples/Views/SaveFilePickerView.xaml.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SaveFilePickerView.xaml.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls.Examples.Views
+{
+    public partial class SaveFilePickerView
+    {
+        public SaveFilePickerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
