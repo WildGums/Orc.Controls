5e609494e71b2bcdfa070588ffe1ad708a4b8313
Tanya Pristupova <zyrk@zyrk.net>
2015-05-26 14:39:45 +0100

    ORCOMP-134 Adds OpenFilePickerView

---
+++ src/Orc.Controls.Examples/ViewModels/OpenFilePickerViewModel.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpenFilePickerViewModel.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls.Examples.ViewModels
+{
+    using Catel.MVVM;
+
+    public class OpenFilePickerViewModel : ViewModelBase
+    {
+        public OpenFilePickerViewModel()
+        {
+        }
+    }
+}
---
+++ src/Orc.Controls.Examples/Views/OpenFilePickerView.xaml.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpenFilePickerView.xaml.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls.Examples.Views
+{
+    public partial class OpenFilePickerView
+    {
+        public OpenFilePickerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
