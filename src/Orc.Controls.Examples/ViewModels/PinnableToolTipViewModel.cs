7f60e3299e2a651e1e60e1c7c373cb7e65cd8e29
Tanya Pristupova <zyrk@zyrk.net>
2015-07-08 12:05:40 +0200

    ORCOMP-153 Adds PinnableToolTip demo. Fixes theme

---
+++ src/Orc.Controls.Examples/ViewModels/PinnableToolTipViewModel.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PinnableToolTipViewModel.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls.Examples.ViewModels
+{
+    using Catel.MVVM;
+
+    public class PinnableToolTipViewModel : ViewModelBase
+    {
+
+    }
+
+}
