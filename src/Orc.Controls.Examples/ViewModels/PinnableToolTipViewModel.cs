3fc770748f2bbd3b93270132a2b40892944c8d3c
Tanya Pristupova <zyrk@zyrk.net>
2015-07-08 13:27:09 +0200

    ORCOMP-153 Updates demo

--- src/Orc.Controls.Examples/ViewModels/PinnableToolTipViewModel.cs
+++ src/Orc.Controls.Examples/ViewModels/PinnableToolTipViewModel.cs
@@ -7,11 +7,25 @@
 
 namespace Orc.Controls.Examples.ViewModels
 {
+    using System.Collections.Generic;
+    using System.Windows.Media;
     using Catel.MVVM;
 
     public class PinnableToolTipViewModel : ViewModelBase
     {
+        public PinnableToolTipViewModel()
+        {
+            ColorList = new List<Brush>()
+            {
+                new SolidColorBrush(Colors.CornflowerBlue),
+                new SolidColorBrush(Colors.CadetBlue),
+                new SolidColorBrush(Colors.LightSeaGreen),
+                new SolidColorBrush(Colors.Orange),
+                new SolidColorBrush(Colors.OrangeRed),
+                new SolidColorBrush(Colors.Red),
+            };
+        }
 
+        public List<Brush> ColorList{ get; set; }
     }
-
 }
