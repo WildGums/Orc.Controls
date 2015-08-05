89b792c34f818aad6c7e2e4fd7c652d85d55fc61
Tanya Pristupova <zyrk@zyrk.net>
2015-08-05 18:55:07 +0200

    ORCOMP-184 Moves AnimatedGif control from Catel to Orc.Controls

---
+++ src/Orc.Controls.Examples/ViewModels/AnimatedGifViewModel.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AnimatedGifViewModel.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls.Examples.ViewModels
+{
+    using Catel.MVVM;
+
+    public class AnimatedGifViewModel : ViewModelBase
+    {
+        public AnimatedGifViewModel()
+        {
+        }
+    }
+}
