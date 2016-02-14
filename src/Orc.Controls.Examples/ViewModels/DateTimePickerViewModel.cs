38f4d9804c4fd14d4215b7768094cb74170146f4
michu <michalp@op.pl>
2016-02-14 18:06:36 +0100

    Add support for selecting datetime format to DateTimePicker example

--- src/Orc.Controls.Examples/ViewModels/DateTimePickerViewModel.cs
+++ src/Orc.Controls.Examples/ViewModels/DateTimePickerViewModel.cs
@@ -8,7 +8,12 @@
 namespace Orc.Controls.Examples.ViewModels
 {
     using System;
+    using Catel.Collections;
+    using System.Collections.ObjectModel;
+    using System.Globalization;
+    using System.Threading.Tasks;
     using Catel.MVVM;
+    using Catel.Threading;
 
     public class DateTimePickerViewModel : ViewModelBase
     {
@@ -21,6 +26,36 @@ namespace Orc.Controls.Examples.ViewModels
 
         #region Properties
         public DateTime DateTimeValue { get; set; }
+        public FastObservableCollection<object> AvailableFormats { get; private set; }
+        public object SelectedFormat { get; set; }
+        #endregion
+
+        #region Methods
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            AvailableFormats = new FastObservableCollection<object>();
+            using (AvailableFormats.SuspendChangeNotifications())
+            {
+                foreach (var cultureInfo in CultureInfo.GetCultures(CultureTypes.AllCultures))
+                {
+                    var format = new
+                    {
+                        CultureCode = string.Format("[{0}]", cultureInfo.IetfLanguageTag),
+                        FormatValue = cultureInfo.DateTimeFormat.ShortDatePattern + " " + cultureInfo.DateTimeFormat.LongTimePattern
+                    };
+
+                    AvailableFormats.Add(format);
+                    if (cultureInfo.Equals(CultureInfo.CurrentCulture))
+                    {
+                        SelectedFormat = format;
+                    }
+                }
+            }
+
+            await TaskHelper.Completed;
+        }
         #endregion
     }
 }
