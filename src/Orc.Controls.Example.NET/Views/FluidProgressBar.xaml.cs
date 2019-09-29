a8d98c5a371c4f635d5fbbc7be50922a6562230d
Vladimir <work.lvg@gmail.com>
2019-09-29 15:05:33 +0400

    Examples of new controls

---
+++ src/Orc.Controls.Example.NET/Views/FluidProgressBar.xaml.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FluidProgressBar.xaml.cs" company="WildGums">
+//   Copyright (c) 2008 - 2019 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Controls.Example.Views
+{
+    /// <summary>
+    /// Interaction logic for FluidProgressBar.xaml
+    /// </summary>
+    public partial class FluidProgressBar
+    {
+        #region Constructors
+        public FluidProgressBar()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
