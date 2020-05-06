868b174f9199e54ed8df4ba62019f70c1484575f
Geert van Horrik <geert@catenalogic.com>
2020-05-06 21:59:12 +0200

    Improve Orc.Theming usage

--- src/Orc.Controls.Example/ViewModels/MainViewModel.cs
+++ src/Orc.Controls.Example/ViewModels/MainViewModel.cs
@@ -7,45 +7,19 @@
 
 namespace Orc.Controls.Example.ViewModels
 {
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Windows;
-    using System.Windows.Media;
-    using Catel;
     using Catel.MVVM;
-    using Catel.Reflection;
-    using ControlzEx.Theming;
 
     public class MainViewModel : ViewModelBase
     {
         #region Constructors
         public MainViewModel()
         {
-            AccentColors = typeof(Colors).GetPropertiesEx(true, true).Where(x => x.PropertyType.IsAssignableFromEx(typeof(Color))).Select(x => (Color)x.GetValue(null)).ToList();
-            SelectedAccentColor = Colors.Orange;
-
             DeferValidationUntilFirstSaveCall = false;
         }
         #endregion
 
         #region Properties
         public override string Title => "Orc.Controls example";
-
-        public List<Color> AccentColors { get; private set; }
-
-        public Color SelectedAccentColor { get; set; }
-        #endregion
-
-        #region Methods
-        private void OnSelectedAccentColorChanged()
-        {
-            var application = Application.Current;
-            var themeManager = ControlzEx.Theming.ThemeManager.Current;
-            var themeGenerator = RuntimeThemeGenerator.Current;
-
-            var generatedTheme = themeGenerator.GenerateRuntimeTheme("Light", SelectedAccentColor);
-            themeManager.ChangeTheme(application, generatedTheme);
-        }
         #endregion
     }
 }
