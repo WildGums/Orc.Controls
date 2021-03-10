e228b00742d2ba75f18dc74cc2a051686b696181
matyesz <matyesz@infra.misoft.hu>
2021-03-10 23:02:37 +0100

    Bugfix 2.0

--- src/Orc.Controls.Example/ViewModels/SideNavigationWizardViewModel.cs
+++ src/Orc.Controls.Example/ViewModels/SideNavigationWizardViewModel.cs
@@ -1,11 +1,20 @@
 ﻿namespace Orc.Controls.Example.ViewModels
 {
+    using System;
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+    using Catel;
+    using Catel.Data;
+    using Catel.MVVM;
     using Catel.Services;
 
-    public class SideNavigationWizardViewModel
+    public class SideNavigationWizardViewModel : ViewModelBase
     {
         public SideNavigationWizardViewModel()
         {
         }
+
+        
     }
 }
--- src/Orc.Controls.Example/Views/SideNavigationWizardWindow.xaml.cs
+++ src/Orc.Controls.Example/Views/SideNavigationWizardWindow.xaml.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -31,89 +32,93 @@
         //{
         //}
 
-        //        public SideNavigationWizardWindow(SideNavigationWizardViewModel viewModel)
-        //            : base(viewModel, DataWindowMode.Custom, infoBarMessageControlGenerationMode: InfoBarMessageControlGenerationMode.Overlay)
-        //        {
-        //            InitializeComponent();
-        //        }
+        //public SideNavigationWizardWindow(SideNavigationWizardViewModel viewModel)
+        //    : base(viewModel, DataWindowMode.Custom, infoBarMessageControlGenerationMode: InfoBarMessageControlGenerationMode.Overlay)
+        //{
+        //    InitializeComponent();
+        //}
 
-        //        protected override void OnLoaded(EventArgs e)
-        //        {
-        //            base.OnLoaded(e);
+        //protected override void OnLoaded(EventArgs e)
+        //{
+        //    base.OnLoaded(e);
 
-        //            Dispatcher.BeginInvoke(() =>
-        //            {
-        //                UpdateOpacityMask();
-        //            });
-        //        }
+        //    Dispatcher.BeginInvoke((Action)(() =>
+        //    {
+        //        UpdateOpacityMask();
+        //    }));
+        //}
 
-        //        protected override void OnViewModelChanged()
-        //        {
-        //            base.OnViewModelChanged();
+        protected override void OnViewModelChanged()
+        {
+            base.OnViewModelChanged();
 
-        //            if (ViewModel is SideNavigationWizardViewModel vm)
-        //            {
-        //                if (vm.Wizard is SideNavigationWizardBase sideNavigationWizard)
-        //                {
-        //                    if (sideNavigationWizard.ShowFullScreen)
-        //                    {
-        //                        SetCurrentValue(WindowStateProperty, WindowState.Maximized);
-        //                        SetCurrentValue(WindowStyleProperty, WindowStyle.None);
-        //                        SetCurrentValue(BorderThicknessProperty, new Thickness(0));
-        //                    }
-        //                }
-        //            }
-        //        }
+            //if (ViewModel is SideNavigationWizardViewModel vm)
+            //{
+            //    if (vm.Wizard is SideNavigationWizardBase sideNavigationWizard)
+            //    {
+            //        if (sideNavigationWizard.ShowFullScreen)
+            //        {
+            //            SetCurrentValue(WindowStateProperty, WindowState.Maximized);
+            //            SetCurrentValue(WindowStyleProperty, WindowStyle.None);
+            //            SetCurrentValue(BorderThicknessProperty, new Thickness(0));
+            //        }
+            //    }
+            //}
+        }
 
-        //        protected override void OnViewModelPropertyChanged(PropertyChangedEventArgs e)
-        //        {
-        //            base.OnViewModelPropertyChanged(e);
+//        protected override void OnViewModelPropertyChanged(PropertyChangedEventArgs e)
+//        {
+//            base.OnViewModelPropertyChanged(e);
 
-        //#pragma warning disable WPF1014
-        //            if (e.HasPropertyChanged("CurrentPage"))
-        //#pragma warning restore WPF1014
-        //            {
-        //#pragma warning disable AvoidAsyncVoid
-        //                Dispatcher.BeginInvoke(async () =>
-        //                {
-        //#pragma warning restore AvoidAsyncVoid
-        //                    var vm = (SideNavigationWizardViewModel)ViewModel;
 
-        //                    stepbar.stepbarListBox.CenterSelectedItem();
-        //                    stepbar.stepbarProgress.UpdateProgress(vm.Wizard.CurrentPage.Number, vm.Wizard.Pages.Count());
 
-        //                    // We need to await the animation
-        //                    await TaskShim.Delay(WizardConfiguration.AnimationDuration);
+//#pragma warning disable WPF1014
+//            if (e.HasPropertyChanged("CurrentPage"))
+//#pragma warning restore WPF1014
+//            {
+//#pragma warning disable AvoidAsyncVoid
+//                Dispatcher.BeginInvoke((Action)(async () =>
+//                {
+//#pragma warning restore AvoidAsyncVoid
+//                    var vm = (SideNavigationWizardViewModel)ViewModel;
+//                    //ListBox stepbarListBox = (ListBox)stepBarItem.GetType().GetField("listBox").GetValue(this);
+//                    //ProgressBar stepbarProgress = (ProgressBar)stepBarItem.GetType().GetField("progressBar").GetValue(this);
 
-        //                    UpdateOpacityMask();
-        //                });
-        //            }
-        //        }
+//                    stepbar.stepbarListBox.CenterSelectedItem();
+//                    stepbar.stepbarProgress.UpdateProgress(vm.Wizard.CurrentPage.Number, vm.Wizard.Pages.Count());
 
-        //        private void UpdateOpacityMask()
-        //        {
-        //            var scrollViewer = stepbar.FindVisualDescendantByType<ScrollViewer>();
-        //            if (scrollViewer is null)
-        //            {
-        //                return;
-        //            }
+//                    // We need to await the animation
+//                    await TaskShim.Delay(WizardConfiguration.AnimationDuration);
 
-        //            var opacityMask = new LinearGradientBrush();
-        //            if (scrollViewer.HorizontalOffset > 0d)
-        //            {
-        //                opacityMask.GradientStops.Add(new GradientStop(Colors.Transparent, 0d));
-        //                opacityMask.GradientStops.Add(new GradientStop(Colors.Black, 0.05d));
-        //            }
+//                    UpdateOpacityMask();
+//                }));
+//            }
+//        }
 
-        //            var scrollableWidth = scrollViewer.ScrollableWidth;
-        //            if (scrollableWidth > scrollViewer.HorizontalOffset)
-        //            {
-        //                opacityMask.GradientStops.Add(new GradientStop(Colors.Black, 0.95d));
-        //                opacityMask.GradientStops.Add(new GradientStop(Colors.Transparent, 1d));
-        //            }
+//        private void UpdateOpacityMask()
+//        {
+//            var scrollViewer = stepbar.FindVisualDescendantByType<ScrollViewer>();
+//            if (scrollViewer is null)
+//            {
+//                return;
+//            }
 
-        //            stepbar.SetCurrentValue(OpacityMaskProperty, opacityMask.GradientStops.Count > 0 ? opacityMask : null);
-        //        }
+//            var opacityMask = new LinearGradientBrush();
+//            if (scrollViewer.HorizontalOffset > 0d)
+//            {
+//                opacityMask.GradientStops.Add(new GradientStop(Colors.Transparent, 0d));
+//                opacityMask.GradientStops.Add(new GradientStop(Colors.Black, 0.05d));
+//            }
+
+//            var scrollableWidth = scrollViewer.ScrollableWidth;
+//            if (scrollableWidth > scrollViewer.HorizontalOffset)
+//            {
+//                opacityMask.GradientStops.Add(new GradientStop(Colors.Black, 0.95d));
+//                opacityMask.GradientStops.Add(new GradientStop(Colors.Transparent, 1d));
+//            }
+
+//            stepbar.SetCurrentValue(OpacityMaskProperty, opacityMask.GradientStops.Count > 0 ? opacityMask : null);
+//        }
     }
 }
 
