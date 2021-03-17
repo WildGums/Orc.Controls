6f9217fca44cc2532b2fe35a878fb67432d0bb95
Mátyás Basa <matiasbasa@gmail.com>
2021-03-17 16:04:41 +0100

    Add CalloutView for Callout example.

---
+++ src/Orc.Controls.Example/Views/CalloutView.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace Orc.Controls.Example.Views
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for CalloutView.xaml
+    /// </summary>
+    public partial class CalloutView
+    {
+        #region Constructors
+        public CalloutView()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
