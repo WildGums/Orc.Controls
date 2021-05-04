98332830fb33b713266eb5d6ee1415de6fffc894
Vladimir <work.lvg@gmail.com>
2021-05-04 20:17:14 +0300

    (WIP)

--- src/Orc.Controls/Controls/Callout/Views/Callout.xaml.cs
+++ src/Orc.Controls/Controls/Callout/Views/Callout.xaml.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Windows;
+    using System.Windows.Controls.Primitives;
     using System.Windows.Input;
     using System.Windows.Markup;
     using Catel.MVVM.Views;
@@ -17,6 +18,89 @@
         public Callout()
         {
             InitializeComponent();
+
+            Popup.Opened += PopupOnOpened;
+        }
+
+        private void PopupOnOpened(object sender, EventArgs e)
+        {
+            UpdatePopupPosition();
+        }
+
+        protected override void OnLoaded(EventArgs e)
+        {
+            //UpdatePopupPosition();
+        }
+
+        private void UpdatePopupPosition()
+        {
+            var placementTarget = PlacementTarget;
+            if (placementTarget is null)
+            {
+                return;
+            }
+
+            RefreshPolygonStyle();
+            RefreshTailSizes();
+        }
+
+        private void RefreshPolygonStyle()
+        {
+            var styleName = GetTailPolygonStyleResourceName();
+            var style = TryFindResource(styleName);
+            TailPolygon.SetValue(FrameworkElement.StyleProperty, style);
+        }
+
+        private void RefreshTailSizes()
+        {
+            if (!(PlacementTarget is FrameworkElement placementTargetControl))
+            {
+                return;
+            }
+
+            var placementMode = Placement;
+
+            switch (placementMode)
+            {
+                case PlacementMode.Bottom:
+                case PlacementMode.Top:
+
+                    Popup.SetCurrentValue(Popup.HorizontalOffsetProperty, (placementTargetControl.ActualWidth - ContentBorder.ActualWidth) / 2d + HorizontalOffset);
+                    BorderGapRectangle.SetCurrentValue(WidthProperty, TailBaseWidth - 4);
+
+                    break;
+
+                case PlacementMode.Right:
+                    Popup.SetCurrentValue(Popup.HorizontalOffsetProperty, (placementTargetControl.ActualWidth - ContentBorder.ActualWidth) / 2d + HorizontalOffset);
+                    BorderGapRectangle.SetCurrentValue(WidthProperty, TailBaseWidth - 4);
+
+
+                    //LeftTailColumnDefinition.SetCurrentValue(ColumnDefinition.WidthProperty, horizontalOffset);
+                    //RightTailColumnDefinition.SetCurrentValue(ColumnDefinition.WidthProperty, zeroGridLength);
+                    //BottomTailRowDefinition.SetCurrentValue(RowDefinition.HeightProperty, zeroGridLength);
+                    //TopTailRowDefinition.SetCurrentValue(RowDefinition.HeightProperty, zeroGridLength);
+                    break;
+
+
+
+                case PlacementMode.Left:
+                    //LeftTailColumnDefinition.SetCurrentValue(ColumnDefinition.WidthProperty, zeroGridLength);
+                    //RightTailColumnDefinition.SetCurrentValue(ColumnDefinition.WidthProperty, horizontalOffset);
+                    //BottomTailRowDefinition.SetCurrentValue(RowDefinition.HeightProperty, zeroGridLength);
+                    //TopTailRowDefinition.SetCurrentValue(RowDefinition.HeightProperty, zeroGridLength);
+                    break;
+
+                default:
+                    //NOTE:Vladimir:Will throw if not supported
+                    throw new NotSupportedException($"Callout placement = '{placementMode}' not supported. Supported modes: '{PlacementMode.Left}', '{PlacementMode.Top}', '{PlacementMode.Right}', '{PlacementMode.Bottom}'");
+            }
+        }
+
+        private string GetTailPolygonStyleResourceName()
+        {
+            var tailAlignment = Placement == PlacementMode.Left || Placement == PlacementMode.Right ? (object) TailVerticalAlignment : TailHorizontalAlignment;
+
+            return $"{Placement}{tailAlignment}PolygonStyle";
         }
 
         [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewWins)]
@@ -71,8 +155,8 @@
         }
 
         public static readonly DependencyProperty PlacementTargetProperty =
-            DependencyProperty.Register(nameof(PlacementTarget), typeof(object), typeof(Callout));
-
+            DependencyProperty.Register(nameof(PlacementTarget), typeof(object), typeof(Callout),
+                new PropertyMetadata((sender, args) => ((Callout)sender).OnPlacementTargetChanged(args)));
 
         [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewWins)]
         public bool IsOpen
@@ -116,5 +200,104 @@
 
         public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(nameof(Command), 
             typeof(ICommand), typeof(Callout), new PropertyMetadata(null));
+
+        public PlacementMode Placement
+        {
+            get { return (PlacementMode)GetValue(PlacementProperty); }
+            set { SetValue(PlacementProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlacementProperty = DependencyProperty.Register(
+            nameof(Placement), typeof(PlacementMode), typeof(Callout), new PropertyMetadata(PlacementMode.Bottom,
+                (sender, args) => ((Callout)sender).OnPlacementChanged(args)));
+
+
+        public double VerticalOffset
+        {
+            get { return (double)GetValue(VerticalOffsetProperty); }
+            set { SetValue(VerticalOffsetProperty, value); }
+        }
+
+        public static readonly DependencyProperty VerticalOffsetProperty = DependencyProperty.Register(
+            nameof(VerticalOffset), typeof(double), typeof(Callout), new PropertyMetadata(default(double),
+                (sender, args) => ((Callout)sender).OnVerticalOffsetChanged(args)));
+
+
+        public double HorizontalOffset
+        {
+            get { return (double)GetValue(HorizontalOffsetProperty); }
+            set { SetValue(HorizontalOffsetProperty, value); }
+        }
+
+        public static readonly DependencyProperty HorizontalOffsetProperty = DependencyProperty.Register(
+            nameof(HorizontalOffset), typeof(double), typeof(Callout), new PropertyMetadata(default(double),
+                (sender, args) => ((Callout)sender).OnHorizontalOffsetChanged(args)));
+
+        public double TailBaseWidth
+        {
+            get { return (double)GetValue(TailBaseWidthProperty); }
+            set { SetValue(TailBaseWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty TailBaseWidthProperty = DependencyProperty.Register(
+            nameof(TailBaseWidth), typeof(double), typeof(Callout), new PropertyMetadata(20d,
+                (sender, args) => ((Callout)sender).OnTailBaseWidthChanged(args)));
+
+        public HorizontalAlignment TailHorizontalAlignment
+        {
+            get { return (HorizontalAlignment)GetValue(TailHorizontalAlignmentProperty); }
+            set { SetValue(TailHorizontalAlignmentProperty, value); }
+        }
+
+        public static readonly DependencyProperty TailHorizontalAlignmentProperty = DependencyProperty.Register(
+            nameof(TailHorizontalAlignment), typeof(HorizontalAlignment), typeof(Callout), new PropertyMetadata(default(HorizontalAlignment),
+                (sender, args) => ((Callout)sender).OnTailHorizontalAlignmentChanged(args)));
+
+        public VerticalAlignment TailVerticalAlignment
+        {
+            get { return (VerticalAlignment)GetValue(TailVerticalAlignmentProperty); }
+            set { SetValue(TailVerticalAlignmentProperty, value); }
+        }
+
+        public static readonly DependencyProperty TailVerticalAlignmentProperty = DependencyProperty.Register(
+            nameof(TailVerticalAlignment), typeof(VerticalAlignment), typeof(Callout), new PropertyMetadata(default(VerticalAlignment),
+                (sender, args) => ((Callout)sender).OnTailVerticalAlignmentChanged(args)));
+
+        private void OnTailVerticalAlignmentChanged(DependencyPropertyChangedEventArgs args)
+        {
+            
+        }
+
+
+        private void OnTailHorizontalAlignmentChanged(DependencyPropertyChangedEventArgs args)
+        {
+            //UpdatePopupPosition();
+        }
+
+
+        private void OnTailBaseWidthChanged(DependencyPropertyChangedEventArgs args)
+        {
+            //UpdatePopupPosition();
+        }
+
+        private void OnHorizontalOffsetChanged(DependencyPropertyChangedEventArgs args)
+        {
+          //  UpdatePopupPosition();
+        }
+        
+        private void OnVerticalOffsetChanged(DependencyPropertyChangedEventArgs args)
+        {
+           // UpdatePopupPosition();
+        }
+        
+        private void OnPlacementTargetChanged(DependencyPropertyChangedEventArgs args)
+        {
+           // UpdatePopupPosition();
+        }
+        
+        private void OnPlacementChanged(DependencyPropertyChangedEventArgs args)
+        {
+            //UpdatePopupPosition();
+        }
     }
 }
