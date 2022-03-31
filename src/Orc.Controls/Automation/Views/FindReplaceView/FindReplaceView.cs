b7e1e04bdcde053275f1d21806ccb8f507e727e6
Vladimir <work.lvg@gmail.com>
2022-03-31 12:52:58 +0300

    cover find replace view

--- src/Orc.Controls/Automation/Views/FindReplaceView/FindReplaceView.cs
+++ src/Orc.Controls/Automation/Views/FindReplaceView/FindReplaceView.cs
@@ -13,5 +13,71 @@
         }
 
         private FindReplaceViewMap Map => Map<FindReplaceViewMap>();
+
+        public void Find(string text)
+        {
+            var map = Map;
+
+            var findEdit = map.FindEdit;
+            findEdit.Text = text;
+
+            map.FindNextButton.Click();
+        }
+
+        public void Replace(string text, string replacementText)
+        {
+            var map = Map;
+
+            var findEdit = map.FindReplaceEdit;
+            findEdit.Text = text;
+
+            var replaceEdit = map.ReplaceEdit;
+            replaceEdit.Text = replacementText;
+
+            map.ReplaceButton.Click();
+        }
+
+        public void ReplaceAll(string text, string replacementText)
+        {
+            var map = Map;
+
+            var findEdit = map.FindReplaceEdit;
+            findEdit.Text = text;
+
+            var replaceEdit = map.ReplaceEdit;
+            replaceEdit.Text = replacementText;
+
+            map.ReplaceAllButton.Click();
+        }
+
+        public bool IsCaseSensitive
+        {
+            get => Map.CaseSensitiveCheckBox.IsChecked == true;
+            set => Map.CaseSensitiveCheckBox.IsChecked = value;
+        }
+
+        public bool IsWholeWord
+        {
+            get => Map.WholeWordCheckBox.IsChecked == true;
+            set => Map.WholeWordCheckBox.IsChecked = value;
+        }
+
+        public bool IsRegex
+        {
+            get => Map.RegexCheckBox.IsChecked == true;
+            set => Map.RegexCheckBox.IsChecked = value;
+        }
+
+        public bool IsWildcards
+        {
+            get => Map.WildcardsCheckBox.IsChecked == true;
+            set => Map.WildcardsCheckBox.IsChecked = value;
+        }
+
+        public bool SearchUp
+        {
+            get => Map.SearchUpCheckBox.IsChecked == true;
+            set => Map.SearchUpCheckBox.IsChecked = value;
+        }
     }
 }
