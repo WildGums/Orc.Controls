7636fc2de0bba6918f8d3562772a2c72b6caacef
Geert van Horrik <geert@catenalogic.com>
2022-10-12 09:16:28 +0200

    Fix build

--- src/Orc.Controls/Controls/Callout/Extensions/IConfigurationServiceExtensions.cs
+++ src/Orc.Controls/Controls/Callout/Extensions/IConfigurationServiceExtensions.cs
@@ -6,13 +6,13 @@
 
     public static class IConfigurationServiceExtensions
     {
-        public static Task<bool> IsCalloutMarkedAsShownAsync(this IConfigurationService configurationService, ICallout callout)
+        public static async Task<bool> IsCalloutMarkedAsShownAsync(this IConfigurationService configurationService, ICallout callout)
         {
             ArgumentNullException.ThrowIfNull(configurationService);
             ArgumentNullException.ThrowIfNull(callout);
 
             var configurationKey = $"{callout.GetCalloutConfigurationKeyPrefix()}.Shown";
-            return configurationService.GetRoamingValueAsync(configurationKey, false);
+            return configurationService.GetRoamingValue(configurationKey, false);
         }
 
         public static Task SetCalloutLastShownAsync(this IConfigurationService configurationService, ICallout callout)
@@ -23,31 +23,31 @@
             return SetCalloutLastShownAsync(configurationService, callout, DateTime.UtcNow);
         }
 
-        public static Task SetCalloutLastShownAsync(this IConfigurationService configurationService, ICallout callout, DateTime? lastShown)
+        public static async Task SetCalloutLastShownAsync(this IConfigurationService configurationService, ICallout callout, DateTime? lastShown)
         {
             ArgumentNullException.ThrowIfNull(configurationService);
             ArgumentNullException.ThrowIfNull(callout);
 
             var configurationKey = $"{callout.GetCalloutConfigurationKeyPrefix()}.LastShown";
-            return configurationService.SetRoamingValueAsync(configurationKey, lastShown);
+            configurationService.SetRoamingValue(configurationKey, lastShown);
         }
 
-        public static Task<DateTime?> GetCalloutLastShownAsync(this IConfigurationService configurationService, ICallout callout)
+        public static async Task<DateTime?> GetCalloutLastShownAsync(this IConfigurationService configurationService, ICallout callout)
         {
             ArgumentNullException.ThrowIfNull(configurationService);
             ArgumentNullException.ThrowIfNull(callout);
 
             var configurationKey = $"{callout.GetCalloutConfigurationKeyPrefix()}.LastShown";
-            return configurationService.GetRoamingValueAsync<DateTime?>(configurationKey, null);
+            return configurationService.GetRoamingValue<DateTime?>(configurationKey, null);
         }
 
-        public static Task MarkCalloutAsNotShownAsync(this IConfigurationService configurationService, ICallout callout)
+        public static async Task MarkCalloutAsNotShownAsync(this IConfigurationService configurationService, ICallout callout)
         {
             ArgumentNullException.ThrowIfNull(configurationService);
             ArgumentNullException.ThrowIfNull(callout);
 
             var configurationKey = $"{callout.GetCalloutConfigurationKeyPrefix()}.Shown";
-            return configurationService.SetRoamingValueAsync(configurationKey, false);
+            configurationService.SetRoamingValue(configurationKey, false);
         }
 
         public static async Task MarkCalloutAsShownAsync(this IConfigurationService configurationService, ICallout callout)
@@ -56,7 +56,7 @@
             ArgumentNullException.ThrowIfNull(callout);
 
             var configurationKey = $"{callout.GetCalloutConfigurationKeyPrefix()}.Shown";
-            await configurationService.SetRoamingValueAsync(configurationKey, true);
+            configurationService.SetRoamingValue(configurationKey, true);
 
             await configurationService.SetCalloutLastShownAsync(callout, DateTime.UtcNow);
         }
