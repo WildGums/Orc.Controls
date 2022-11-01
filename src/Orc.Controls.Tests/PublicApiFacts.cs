6a8ac17b712ed3ad7a1590bead4f0c5a510b5271
Dmitry-VF <fomedima@gmail.com>
2022-11-02 00:19:46 +0300

    Migrate to Verify for API approvals

--- src/Orc.Controls.Tests/PublicApiFacts.cs
+++ src/Orc.Controls.Tests/PublicApiFacts.cs
@@ -10,45 +10,28 @@ namespace Orc.Controls.Tests
     using System.IO;
     using System.Reflection;
     using System.Runtime.CompilerServices;
-    using ApprovalTests;
-    using ApprovalTests.Namers;
+    using System.Threading.Tasks;
     using NUnit.Framework;
     using PublicApiGenerator;
+    using VerifyNUnit;
 
     [TestFixture]
     public class PublicApiFacts
     {
         [Test, MethodImpl(MethodImplOptions.NoInlining)]
-        public void Orc_Controls_HasNoBreakingChanges()
+        public async Task Orc_Controls_HasNoBreakingChanges_Async()
         {
             var assembly = typeof(ResizingAdorner).Assembly;
 
-            PublicApiApprover.ApprovePublicApi(assembly);
+            await PublicApiApprover.ApprovePublicApiAsync(assembly);
         }
 
         internal static class PublicApiApprover
         {
-            public static void ApprovePublicApi(Assembly assembly)
+            public static async Task ApprovePublicApiAsync(Assembly assembly)
             {
                 var publicApi = ApiGenerator.GeneratePublicApi(assembly, new ApiGeneratorOptions());
-                var writer = new ApprovalTextWriter(publicApi, "cs");
-                var approvalNamer = new AssemblyPathNamer(assembly.Location);
-                Approvals.Verify(writer, approvalNamer, Approvals.GetReporter());
-            }
-        }
-
-        internal class AssemblyPathNamer : UnitTestFrameworkNamer
-        {
-            private readonly string _name;
-
-            public AssemblyPathNamer(string assemblyPath)
-            {
-                _name = Path.GetFileNameWithoutExtension(assemblyPath);
-            }
-
-            public override string Name
-            {
-                get { return _name; }
+                await Verifier.Verify(publicApi);
             }
         }
     }
