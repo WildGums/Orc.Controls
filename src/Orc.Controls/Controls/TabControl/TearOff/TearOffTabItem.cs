c9f6b15b917ccd3dd1c33b7f402fdf5b271c6eaa
Vladimir <work.lvg@gmail.com>
2025-08-15 20:58:29 +0300

    tear off tabs

---
+++ src/Orc.Controls/Controls/TabControl/TearOff/TearOffTabItem.cs
@@ -0,0 +1,238 @@
+﻿namespace Orc.Controls;
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+/// <summary>
+/// TabItem that supports tear-off functionality
+/// </summary>
+public class TearOffTabItem : TabItem
+{
+    private bool _isDragging;
+    private Point _startPoint;
+    private TearOffWindow? _tearOffWindow;
+
+    /// <summary>
+    /// Dependency property for IsTornOff
+    /// </summary>
+    public static readonly DependencyProperty IsTornOffProperty =
+        DependencyProperty.Register(nameof(IsTornOff), typeof(bool), typeof(TearOffTabItem),
+            new PropertyMetadata(false, OnIsTornOffChanged));
+
+    /// <summary>
+    /// Dependency property for TearOffThreshold
+    /// </summary>
+    public static readonly DependencyProperty TearOffThresholdProperty =
+        DependencyProperty.Register(nameof(TearOffThreshold), typeof(double), typeof(TearOffTabItem),
+            new PropertyMetadata(20.0));
+
+    /// <summary>
+    /// Dependency property for CanTearOff
+    /// </summary>
+    public static readonly DependencyProperty CanTearOffProperty =
+        DependencyProperty.Register(nameof(CanTearOff), typeof(bool), typeof(TearOffTabItem),
+            new PropertyMetadata(true));
+
+    /// <summary>
+    /// Event raised when a tab is torn off
+    /// </summary>
+    public static readonly RoutedEvent TabTornOffEvent =
+        EventManager.RegisterRoutedEvent(nameof(TabTornOff), RoutingStrategy.Bubble,
+            typeof(TearOffEventHandler), typeof(TearOffTabItem));
+
+    /// <summary>
+    /// Event raised when a tab is docked back
+    /// </summary>
+    public static readonly RoutedEvent TabDockedEvent =
+        EventManager.RegisterRoutedEvent(nameof(TabDocked), RoutingStrategy.Bubble,
+            typeof(TearOffEventHandler), typeof(TearOffTabItem));
+
+    static TearOffTabItem()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(TearOffTabItem),
+            new FrameworkPropertyMetadata(typeof(TearOffTabItem)));
+    }
+
+    /// <summary>
+    /// Gets or sets whether this tab is currently torn off
+    /// </summary>
+    public bool IsTornOff
+    {
+        get => (bool)GetValue(IsTornOffProperty);
+        set => SetValue(IsTornOffProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the distance threshold for tear-off to activate
+    /// </summary>
+    public double TearOffThreshold
+    {
+        get => (double)GetValue(TearOffThresholdProperty);
+        set => SetValue(TearOffThresholdProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets whether this tab can be torn off
+    /// </summary>
+    public bool CanTearOff
+    {
+        get => (bool)GetValue(CanTearOffProperty);
+        set => SetValue(CanTearOffProperty, value);
+    }
+
+    /// <summary>
+    /// Event raised when tab is torn off
+    /// </summary>
+    public event TearOffEventHandler TabTornOff
+    {
+        add => AddHandler(TabTornOffEvent, value);
+        remove => RemoveHandler(TabTornOffEvent, value);
+    }
+
+    /// <summary>
+    /// Event raised when tab is docked back
+    /// </summary>
+    public event TearOffEventHandler TabDocked
+    {
+        add => AddHandler(TabDockedEvent, value);
+        remove => RemoveHandler(TabDockedEvent, value);
+    }
+
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        if (CanTearOff && !IsTornOff)
+        {
+            _startPoint = e.GetPosition(this);
+            _isDragging = true;
+            CaptureMouse();
+        }
+
+        base.OnMouseLeftButtonDown(e);
+    }
+
+    protected override void OnMouseMove(MouseEventArgs e)
+    {
+        if (_isDragging && e.LeftButton == MouseButtonState.Pressed && CanTearOff && !IsTornOff)
+        {
+            var currentPoint = e.GetPosition(this);
+            var distance = (currentPoint - _startPoint).Length;
+
+            if (distance > TearOffThreshold)
+            {
+                InitiateTearOff();
+            }
+        }
+
+        base.OnMouseMove(e);
+    }
+
+    protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+    {
+        if (_isDragging)
+        {
+            _isDragging = false;
+            ReleaseMouseCapture();
+        }
+
+        base.OnMouseLeftButtonUp(e);
+    }
+
+    private void InitiateTearOff()
+    {
+        _isDragging = false;
+        ReleaseMouseCapture();
+
+        var tabControl = FindParent<TabControl>(this);
+        if (tabControl is null)
+            return;
+
+        var tearOffEventArgs = new TearOffEventArgs(TabTornOffEvent, this);
+        RaiseEvent(tearOffEventArgs);
+
+        if (!tearOffEventArgs.Handled)
+        {
+            PerformTearOff();
+        }
+    }
+
+    private void PerformTearOff()
+    {
+        var content = Content;
+        var header = Header;
+
+        // Create tear-off window
+        _tearOffWindow = new TearOffWindow(this, content, header?.ToString() ?? "Torn Tab");
+        _tearOffWindow.Show();
+        _tearOffWindow.Closed += OnTearOffWindowClosed;
+
+        // Mark as torn off
+        SetCurrentValue(IsTornOffProperty, true);
+
+        // Hide the content in the original tab
+        SetCurrentValue(ContentProperty, null);
+        SetCurrentValue(VisibilityProperty, Visibility.Collapsed);
+    }
+
+    private void OnTearOffWindowClosed(object? sender, EventArgs e)
+    {
+        if (_tearOffWindow is not null)
+        {
+            _tearOffWindow.Closed -= OnTearOffWindowClosed;
+
+            // If window was closed by docking, don't restore content here
+            if (!_tearOffWindow.WasDockedBack)
+            {
+                DockBack(_tearOffWindow.OriginalContent);
+            }
+
+            _tearOffWindow = null;
+        }
+    }
+
+    /// <summary>
+    /// Docks the tab back from its torn-off state
+    /// </summary>
+    public void DockBack(object? content = null)
+    {
+        if (!IsTornOff)
+            return;
+
+        // Restore content
+        SetCurrentValue(ContentProperty, content ?? _tearOffWindow?.OriginalContent);
+        SetCurrentValue(VisibilityProperty, Visibility.Visible);
+        SetCurrentValue(IsTornOffProperty, false);
+
+        // Close tear-off window if it exists
+        if (_tearOffWindow is not null)
+        {
+            _tearOffWindow.WasDockedBack = true;
+            _tearOffWindow.Close();
+        }
+
+        var tearOffEventArgs = new TearOffEventArgs(TabDockedEvent, this);
+        RaiseEvent(tearOffEventArgs);
+    }
+
+    private static void OnIsTornOffChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is TearOffTabItem tabItem && (bool)e.NewValue)
+        {
+            tabItem.IsSelected = false;
+        }
+    }
+
+    private static T? FindParent<T>(DependencyObject child) where T : DependencyObject
+    {
+        var parentObject = VisualTreeHelper.GetParent(child);
+
+        return parentObject switch
+        {
+            null => null,
+            T parent => parent,
+            _ => FindParent<T>(parentObject)
+        };
+    }
+}
---
+++ src/Orc.Controls/Controls/TabControl/TearOff/TearOffWindow.cs
@@ -0,0 +1,317 @@
+﻿namespace Orc.Controls;
+
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using Application = System.Windows.Application;
+using Button = System.Windows.Controls.Button;
+using Cursors = System.Windows.Input.Cursors;
+using HorizontalAlignment = System.Windows.HorizontalAlignment;
+using MessageBox = System.Windows.MessageBox;
+using MouseEventArgs = System.Windows.Input.MouseEventArgs;
+
+/// <summary>
+/// Window that hosts a torn-off tab content
+/// </summary>
+public class TearOffWindow : Window
+{
+    /// <summary>
+    /// Dependency property for dock back drop zones
+    /// </summary>
+    public static readonly DependencyProperty AllowDockBackProperty =
+        DependencyProperty.Register(nameof(AllowDockBack), typeof(bool), typeof(TearOffWindow),
+            new(true));
+
+    private readonly TearOffTabItem _originalTabItem;
+    private bool _isDockingBack;
+
+    public TearOffWindow(TearOffTabItem originalTabItem, object? content, string title)
+    {
+        _originalTabItem = originalTabItem ?? throw new ArgumentNullException(nameof(originalTabItem));
+
+        InitializeWindow(content, title);
+        SetupDragAndDrop();
+    }
+
+    /// <summary>
+    /// Gets or sets whether this window allows docking back
+    /// </summary>
+    public bool AllowDockBack
+    {
+        get => (bool)GetValue(AllowDockBackProperty);
+        set => SetValue(AllowDockBackProperty, value);
+    }
+
+    /// <summary>
+    /// Gets the original content that was in the tab
+    /// </summary>
+    public object? OriginalContent { get; private set; }
+
+    /// <summary>
+    /// Gets whether this window was closed due to docking back
+    /// </summary>
+    public bool WasDockedBack { get; set; }
+
+    private void InitializeWindow(object? content, string title)
+    {
+        SetCurrentValue(TitleProperty, title);
+        SetCurrentValue(ContentProperty, content);
+        OriginalContent = content;
+
+        SetCurrentValue(WidthProperty, (double)600);
+        SetCurrentValue(HeightProperty, (double)400);
+        WindowStartupLocation = WindowStartupLocation.Manual;
+
+        // Position near mouse cursor
+        var mousePos = GetMousePosition();
+        SetCurrentValue(LeftProperty, mousePos.X - Width / 2);
+        SetCurrentValue(TopProperty, mousePos.Y - 50);
+
+        // Ensure window is on screen
+        EnsureWindowOnScreen();
+
+        // Style the window
+        SetCurrentValue(WindowStyleProperty, WindowStyle.SingleBorderWindow);
+        SetCurrentValue(ResizeModeProperty, ResizeMode.CanResize);
+        SetCurrentValue(ShowInTaskbarProperty, true);
+
+        // Add dock back button to window
+        if (AllowDockBack)
+        {
+            AddDockBackButton();
+        }
+    }
+
+    private void AddDockBackButton()
+    {
+        var grid = new Grid();
+
+        var dockButton = new Button
+        {
+            Content = "Dock Back",
+            Width = 80,
+            Height = 25,
+            HorizontalAlignment = HorizontalAlignment.Right,
+            VerticalAlignment = VerticalAlignment.Top,
+            Margin = new(0, 5, 5, 0)
+        };
+
+        dockButton.Click += OnDockBackButtonClick;
+
+        // Wrap original content
+        var contentBorder = new Border
+        {
+            Margin = new(5, 35, 5, 5)
+        };
+
+        if (Content is UIElement originalContent)
+        {
+            contentBorder.Child = originalContent;
+        }
+
+        grid.Children.Add(contentBorder);
+        grid.Children.Add(dockButton);
+
+        SetCurrentValue(ContentProperty, grid);
+    }
+
+    private void OnDockBackButtonClick(object sender, RoutedEventArgs e)
+    {
+        DockBack();
+    }
+
+    private void SetupDragAndDrop()
+    {
+        MouseLeftButtonDown += OnMouseLeftButtonDown;
+        MouseMove += OnMouseMove;
+        MouseLeftButtonUp += OnMouseLeftButtonUp;
+    }
+
+    private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (AllowDockBack && e.ClickCount == 1)
+        {
+            DragMove();
+        }
+        else if (e.ClickCount == 2)
+        {
+            // Double-click to dock back
+            DockBack();
+        }
+    }
+
+    private void OnMouseMove(object sender, MouseEventArgs e)
+    {
+        if (AllowDockBack && e.LeftButton == MouseButtonState.Pressed && !_isDockingBack)
+        {
+            // Check if we're over a valid drop target (TabControl)
+            CheckForDockTarget();
+        }
+    }
+
+    private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        if (_isDockingBack)
+        {
+            return;
+        }
+
+        if (AllowDockBack)
+        {
+            // Check if we should dock back
+            var dropTarget = GetDropTarget();
+            if (dropTarget is not null)
+            {
+                DockBack();
+            }
+        }
+    }
+
+    private void CheckForDockTarget()
+    {
+        var dropTarget = GetDropTarget();
+        if (dropTarget is not null)
+        {
+            // Visual feedback could be added here
+            SetCurrentValue(CursorProperty, Cursors.Hand);
+        }
+        else
+        {
+            SetCurrentValue(CursorProperty, Cursors.Arrow);
+        }
+    }
+
+    private TabControl? GetDropTarget()
+    {
+        var mousePos = GetMousePosition();
+        var elementUnderMouse = GetElementUnderMouse(mousePos);
+
+        // Walk up the visual tree to find a TabControl
+        var current = elementUnderMouse;
+        while (current is not null)
+        {
+            if (current is TabControl tabControl)
+            {
+                return tabControl;
+            }
+
+            current = VisualTreeHelper.GetParent(current);
+        }
+
+        return null;
+    }
+
+    private DependencyObject? GetElementUnderMouse(Point screenPoint)
+    {
+        // Convert screen coordinates to find element
+        foreach (Window window in Application.Current.Windows)
+        {
+            if (window == this)
+            {
+                continue;
+            }
+
+            try
+            {
+                var windowPoint = window.PointFromScreen(screenPoint);
+                if (windowPoint.X >= 0 && windowPoint.Y >= 0 &&
+                    windowPoint.X <= window.ActualWidth && windowPoint.Y <= window.ActualHeight)
+                {
+                    return window.InputHitTest(windowPoint) as DependencyObject;
+                }
+            }
+            catch
+            {
+                // Ignore hit test failures
+            }
+        }
+
+        return null;
+    }
+
+    private Point GetMousePosition()
+    {
+        var point = Mouse.GetPosition(Application.Current.MainWindow);
+        return Application.Current.MainWindow?.PointToScreen(point) ?? new Point(0, 0);
+    }
+
+    private void EnsureWindowOnScreen()
+    {
+        var screen = Screen.FromPoint(new((int)Left, (int)Top));
+
+        if (Left < screen.WorkingArea.Left)
+        {
+            SetCurrentValue(LeftProperty, (double)screen.WorkingArea.Left);
+        }
+
+        if (Top < screen.WorkingArea.Top)
+        {
+            SetCurrentValue(TopProperty, (double)screen.WorkingArea.Top);
+        }
+
+        if (Left + Width > screen.WorkingArea.Right)
+        {
+            SetCurrentValue(LeftProperty, screen.WorkingArea.Right - Width);
+        }
+
+        if (Top + Height > screen.WorkingArea.Bottom)
+        {
+            SetCurrentValue(TopProperty, screen.WorkingArea.Bottom - Height);
+        }
+    }
+
+    /// <summary>
+    /// Docks this window back to its original tab
+    /// </summary>
+    public void DockBack()
+    {
+        if (_isDockingBack)
+        {
+            return;
+        }
+
+        _isDockingBack = true;
+        WasDockedBack = true;
+
+        // Get the content back from the wrapper if needed
+        var contentToRestore = OriginalContent;
+        if (Content is Grid grid && grid.Children.Count > 0)
+        {
+            if (grid.Children[0] is Border border)
+            {
+                contentToRestore = border.Child;
+            }
+        }
+
+        _originalTabItem.DockBack(contentToRestore);
+        Close();
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        if (!WasDockedBack && AllowDockBack)
+        {
+            var result = MessageBox.Show(
+                "Do you want to dock this tab back to the main window?",
+                "Close Tab",
+                MessageBoxButton.YesNoCancel,
+                MessageBoxImage.Question);
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    DockBack();
+                    break;
+                case MessageBoxResult.Cancel:
+                    e.Cancel = true;
+                    return;
+            }
+        }
+
+        base.OnClosing(e);
+    }
+}
