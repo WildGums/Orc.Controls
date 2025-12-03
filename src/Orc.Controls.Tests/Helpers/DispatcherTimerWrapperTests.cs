388d2177f1fea915d3ed0e9106dc996b78b0e621
Vladimir <work.lvg@gmail.com>
2025-12-04 03:16:11 +0700

    Implements a postpone action helper
    Adds a helper class to postpone the execution of an action.
    
    This allows delaying execution using a timer, preventing excessive
    or immediate operations, improving performance and user experience.
    
    The implementation includes an interface for timer abstraction to
    facilitate testing and different timer implementations.

---
+++ src/Orc.Controls.Tests/Helpers/DispatcherTimerWrapperTests.cs
@@ -0,0 +1,70 @@
+﻿namespace Orc.Controls.Tests.Helpers;
+
+using System;
+using System.Threading;
+using NUnit.Framework;
+
+[TestFixture]
+[Apartment(ApartmentState.STA)] // Требуется для DispatcherTimer
+public class DispatcherTimerWrapperTests
+{
+    [Test]
+    public void Constructor_CreatesDispatcherTimer()
+    {
+        // Act
+        var wrapper = new DispatcherTimerWrapper();
+
+        // Assert
+        Assert.That(wrapper, Is.Not.Null);
+    }
+
+    [Test]
+    public void Interval_SetAndGet_ReturnsCorrectValue()
+    {
+        // Arrange
+        var wrapper = new DispatcherTimerWrapper();
+        var interval = TimeSpan.FromMilliseconds(500);
+
+        // Act
+        wrapper.Interval = interval;
+
+        // Assert
+        Assert.That(wrapper.Interval, Is.EqualTo(interval));
+    }
+
+    [Test]
+    public void Start_SetsIsEnabledToTrue()
+    {
+        // Arrange
+        var wrapper = new DispatcherTimerWrapper
+        {
+            Interval = TimeSpan.FromMilliseconds(100)
+        };
+
+        // Act
+        wrapper.Start();
+
+        // Assert
+        Assert.That(wrapper.IsEnabled, Is.True);
+
+        // Cleanup
+        wrapper.Stop();
+    }
+
+    [Test]
+    public void Stop_SetsIsEnabledToFalse()
+    {
+        // Arrange
+        var wrapper = new DispatcherTimerWrapper
+        {
+            Interval = TimeSpan.FromMilliseconds(100)
+        };
+        wrapper.Start();
+
+        // Act
+        wrapper.Stop();
+
+        // Assert
+        Assert.That(wrapper.IsEnabled, Is.False);
+    }
+}
